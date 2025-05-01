05c857f0067ce58ba55dee8eff893940a9fa5ab5
Goatzmyliver <megson.troy@gmail.com>
2025-05-01 14:19:32 +1200

    8

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "next/navigation"
-
-export default function Home() {
-  // Redirect to login page immediately
-  redirect("/login")
-
-  // This won't be rendered due to the redirect
-  return null
-}
